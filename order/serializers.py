7141bcb4249f1c77f3c54c01744ba142660ada2b
jhamez-cloud <jameskekeli12@gmail.com>
2026-07-18 12:09:56 +0000

    feat: order model api endpoint and serialization

---
+++ order/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import OrdersTimeLineEntry,Order,OrderItem
+from menu.serializers import MenuItemReadSerializer,AddonSerializer
+from user.serializers import UserReadSerializer
+
+class OrderTimelineEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrdersTimeLineEntry
+        fields = ['status', 'label', 'time', 'completed']
+        ignore = ['order']
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    subtotal = serializers.ReadOnlyField()
+    menu_item = MenuItemReadSerializer()
+    addons = AddonSerializer(many=True)
+
+    class Meta:
+        model = OrderItem
+        fields = ['menu_item','name','quantity', 'price', 'addons', 'subtotal']
+
+class OrderReadSerializer(serializers.ModelSerializer):
+    order_items = OrderItemSerializer(many=True,read_only=True)
+    user = UserReadSerializer(read_only=True)
+    subtotal = serializers.ReadOnlyField()
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class OrderWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['order_id',
+                  'restaurant',
+                  'user',
+                  'order_items',
+                  'delivery_address',
+                  'payment_method',
+                  'external_payment_id',
+                  'tip','discount',
+                  'delivery_fee',
+                  'status',
+                  'estimated_delivery_time',
+                  'courier'
+                ]

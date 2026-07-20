57c13137ac536d5c926964e98c2bd53d5c09e8ae
jhamez-cloud <jameskekeli12@gmail.com>
2026-07-20 03:35:31 +0000

    API enpoint routes

--- config/api_urls.py
+++ config/api_urls.py
@@ -1,8 +1,14 @@
 from django.urls import path,include
 
 urlpatterns = [
-    path("", include("user.urls")),
-    path("", include("restaurant.urls")),
+    path("",include("user.urls")),
+    path("",include("restaurant.urls")),
     path("",include("menu.urls")),
     path("",include("cart.urls")),
+    path("",include("order.urls")),
+    path("",include("promotion.urls")),
+    path("",include("review.urls")),
+    path("",include("favorite.urls")),
+    path("",include("notification.urls")),
+    path("",include("wallet.urls")),
 ]

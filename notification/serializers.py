c13889b4a66f0bd56e08eab5553f0c4a1be5d7d9
jhamez-cloud <jameskekeli12@gmail.com>
2026-07-22 00:18:57 +0000

    fix: notification serializers import

--- notification/serializers.py
+++ notification/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from .models import Notification
-from user.serializers import UserReadSerializer
+from user.serializers import UserSerializer
 
 
 class NotificationReadSerializer(serializers.ModelSerializer):
-    user = UserReadSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Notification
         fields = '__all__'

3f9b642772d9041b5f95ed42623fc8c35bae50d7
jhamez-cloud <jameskekeli12@gmail.com>
2026-07-23 01:08:16 +0000

    feat: added menuitem addon model , fix FK relationships

--- menu/models.py
+++ menu/models.py
@@ -1,45 +1,45 @@
+import uuid
 from django.db import models
-from django.utils.text import slugify
+from django.core.validators import MinValueValidator
+from config.helpers import generate_unique_slug
 
 # Create your models here.
 class MenuCategory(models.Model):
-    category_id = models.CharField(max_length=255, unique=True, editable=False)
+    category_id = models.UUIDField(default=uuid.uuid4,editable=False,unique=True)
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
 
     def save(self,*args, **kwargs):
-        new = self.pk is None
+        if not self.slug:
+            self.slug = generate_unique_slug(self, self.name)
+
         super().save(*args,**kwargs)
 
-        if new and not self.category_id and not self.slug:
-            self.category_id = f"menu_category_{self.pk:03d}"
-            self.slug = slugify(self.name)
-            MenuCategory.objects.filter(pk=self.pk).update(
-                category_id=self.category_id, 
-                slug=self.slug
-            )
-
 class MenuItem(models.Model):
-    item_id = models.CharField(max_length=255, unique=True, editable=False)
+    item_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     restaurant = models.ForeignKey('restaurant.Restaurant', on_delete=models.CASCADE, related_name='menu_items')
-    category = models.ForeignKey(MenuCategory, on_delete=models.SET_NULL, null=True, related_name='menu_items')
+    category = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, null=True, related_name='menu_items')
     name = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0.00)])
     image = models.ImageField(upload_to='menu/items/', null=True, blank=True)
     available = models.BooleanField(default=True)
     calories = models.PositiveIntegerField(null=True, blank=True)
     is_popular = models.BooleanField(default=False)
-    addons = models.ManyToManyField('Addon', related_name='menu_items', blank=True)
+    addons = models.ManyToManyField('Addon', through='MenuItemAddon', related_name='menu_items', blank=True)
 
-    def save(self,*args, **kwargs):
-        new = self.pk is None
-        super().save(*args,**kwargs)
+class MenuItemAddon(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    addon = models.ForeignKey('Addon', on_delete=models.CASCADE)
+    is_required = models.BooleanField(default=False)
+    max_selectable = models.PositiveIntegerField(
+        default=1, help_text="Max quantity of this addon a customer can select"
+    )
 
-        if new and not self.item_id:
-            self.item_id = f"menu_item_{self.pk:03d}"
-            MenuItem.objects.filter(pk=self.pk).update(item_id=self.item_id)
+    class Meta:
+        unique_together = ('menu_item', 'addon')
 
 class Addon(models.Model):
+    restaurant = models.ForeignKey('restaurant.Restaurant', on_delete=models.CASCADE, related_name='addons')
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
